4dd74cb797e7a060e64c0484a1db3febdc156f27
Atul.Arora <atul.arora@in.imshealth.com>
2020-04-19 21:02:43 +0530

    Updates

---
+++ precache-manifest.26b681a7a9ba4e813627d84b72822f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cac0d2452ec4a85316591e1ecc2c727",
+    "url": "/monters-rolodex/index.html"
+  },
+  {
+    "revision": "aa376422ab1552d7f66b",
+    "url": "/monters-rolodex/static/css/main.662410a5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa376422ab1552d7f66b",
+    "url": "/monters-rolodex/static/js/main.3933d7d8.chunk.js"
+  },
+  {
+    "revision": "d675114f4bc5dcac6e2b",
+    "url": "/monters-rolodex/static/js/runtime-main.e042b115.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
